36b0043988565a5db515dc19bb7fe806add264bc
Ivan <trubnikov_is@gkl-kemerovo.ru>
2023-04-20 15:56:01 +0300

    completed the first task

---
+++ Task_1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите 1 число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2 число");
+int b = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (a>b)
+{
+    max = a;
+}
+else 
+    max = b;
+Console.Write("max = ");
+Console.WriteLine(max);
