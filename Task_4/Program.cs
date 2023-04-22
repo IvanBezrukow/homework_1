514fc5e361bd144cca5b7255d5bda67ed2ced740
Ivan <trubnikov_is@gkl-kemerovo.ru>
2023-04-22 17:12:59 +0300

    completed the forth task

---
+++ Task_4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число = ");
+int num = Convert.ToInt32(Console.ReadLine());
+int a = 1;
+while (a < num + 1)
+    {
+    if (a % 2 == 0)
+    {
+        Console.Write(" --> ");
+        Console.WriteLine(a);
+        a = a + 1;
+    }
+    else 
+        a = a + 1;
+    }
