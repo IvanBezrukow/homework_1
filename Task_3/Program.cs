4f8760c44b2a776dae79a5749a907980bc12ef17
Ivan <trubnikov_is@gkl-kemerovo.ru>
2023-04-23 00:10:50 +0300

    fixed the code of the third tssk

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -1,6 +1,5 @@
-﻿Console.WriteLine("Введите 1 число");
+﻿Console.WriteLine("Введите число");
 int a = Convert.ToInt32(Console.ReadLine());
-
 if (a % 2 == 0)
 {
     Console.WriteLine("Число четное");
