f398f9fe8f179b3d9ec1ffa66d107ce528450b9c
Ivan <trubnikov_is@gkl-kemerovo.ru>
2023-04-23 00:05:39 +0300

    made some changes in the code of the forth task

--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -1,14 +1,17 @@
 ﻿Console.Write("Введите число = ");
 int num = Convert.ToInt32(Console.ReadLine());
 int a = 1;
+if (num > 1)
 while (a < num + 1)
-    {
-    if (a % 2 == 0)
-    {
-        Console.Write(" --> ");
-        Console.WriteLine(a);
-        a = a + 1;
-    }
-    else 
-        a = a + 1;
-    }
+        {
+        if (a % 2 == 0)
+        {
+            Console.Write(" --> ");
+            Console.WriteLine(a);
+            a = a + 1;
+        }
+        else 
+            a = a + 1;
+        }
+else 
+    Console.WriteLine("Введите другое число");
