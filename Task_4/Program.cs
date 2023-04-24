af827982c65d41a566a45f4e196929fe7ecba661
Ivan <trubnikov_is@gkl-kemerovo.ru>
2023-04-24 04:42:25 +0300

    formatted the code

--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -2,16 +2,16 @@
 int num = Convert.ToInt32(Console.ReadLine());
 int a = 1;
 if (num > 1)
-while (a < num + 1)
-        {
+    while (a < num + 1)
+    {
         if (a % 2 == 0)
         {
             Console.Write(" --> ");
             Console.WriteLine(a);
             a = a + 1;
         }
-        else 
+        else
             a = a + 1;
-        }
-else 
+    }
+else
     Console.WriteLine("Введите другое число");
